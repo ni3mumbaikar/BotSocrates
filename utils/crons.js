9700b2ce5f8e0a462f5f47ae3cb7331ef97c2474
Nitin Kale <nitinkale035@gmail.com>
2022-10-08 10:38:47 +0530

    Fix : cron expression

--- utils/crons.js
+++ utils/crons.js
@@ -5,13 +5,13 @@ const cron = require("node-cron");
 const getNews = require("../commands/technews").getNews;
 
 // cron expression
-// 0 19 * * * => for 7:00 PM
-// 0 7 * * *  => for 7:00 AM
+// 0 0 7,19 ? * * * => for 7:00 PM every day  
 
 
 module.exports.setUpCrons = async function (sock) {
 
-    let sendNews = async function () {
+
+    cron.schedule("0 0 7,19 ? * * *", async function () {
         let URI = process.env.MONGO_URI;
         const client = new MongoClient(URI);
         try {
@@ -30,11 +30,7 @@ module.exports.setUpCrons = async function (sock) {
         } finally {
             // client.close();
         }
-    }
-
-    cron.schedule("0 7 * * *", sendNews());
-
-    cron.schedule("0 19 * * *", sendNews());
+    });
 
 }
 
